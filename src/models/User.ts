793e5eaa118a4b5db640ff1841e278b7a585a3f4
Chad Hinds <chadrickj8@gmail.com>
2021-04-22 10:53:54 -0400

    Extract everything around handling events into a seperate class

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,9 +1,32 @@
+import axios, { AxiosResponse } from 'axios';
+
 interface UserProps {
-    name: string,
-    age: number
+    // '?' marks the properties as optional 
+    id?: number,
+    name?: string,
+    age?: number
 }
 
 export class User {
+
     // Set to private so collaborators cannot tinker with it
     constructor(private data: UserProps) { }
+
+    get(propName: string): (number | string) {
+        return this.data[propName];
+    }
+    // Change and update user's properties
+    set(update: UserProps): void {
+        Object.assign(this.data, update);
+    }
+
+
+
+
+    fetch(): void {
+        axios.get(`http://localhost:3000/users/${this.get('id')}`)
+            .then((response: AxiosResponse): void => {
+                this.set(response.data)
+            })
+    }
 }
