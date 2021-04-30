a6ec1af3b5305914c04813cd4ce0733eb6204df6
Chad Hinds <chadrickj8@gmail.com>
2021-04-30 11:27:29 -0400

    Import Attributes

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,7 +1,6 @@
-import axios, { AxiosResponse } from 'axios';
 import { Eventing } from './Eventing';
 import { Sync } from './Sync';
-
+import { Attributes } from './Attributes';
 
 
 export interface UserProps {
@@ -13,8 +12,10 @@ export interface UserProps {
 const rooturl = 'http://localhost:3000/users'
 export class User {
     public events: Eventing = new Eventing()
-
     public sync: Sync<UserProps> = new Sync<UserProps>(rooturl);
+    public attributes: Attributes<UserProps>;
 
-
+    constructor(attrs: UserProps) {
+        this.attributes = new Attributes<UserProps>(attrs);
+    }
 }
