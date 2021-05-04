fa86402245f55e830b04a38187f4767f11eb57ff
Chad Hinds <chadrickj8@gmail.com>
2021-05-04 16:09:00 -0400

    Change comment regarding how passthroughs are being used

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,4 @@ import { User } from './models/User';
 
 const user = new User({ name: 'new record', age: 0 })
 
-// Makes a reference to the on function in the user class
-user.on('change', () => {
-    console.log('User was changed!')
-})
+// Makes a reference to the eventing functions
