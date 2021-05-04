65b2a8edba704145dd99590d65753341d31e4b79
Chad Hinds <chadrickj8@gmail.com>
2021-05-04 10:55:38 -0400

    Makes a reference to the on function in the user class

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,7 @@ import { User } from './models/User';
 
 const user = new User({ name: 'new record', age: 0 })
 
+// Makes a reference to the on function in the user class
+user.on('change', () => {
+    console.log('User was changed!')
+})
