198dee1644d763ca80ca97b6b80261c5c2e945bc
Chad Hinds <chadrickj8@gmail.com>
2021-05-06 11:13:52 -0400

    Add the on and the trigger methods

--- src/models/Collection.ts
+++ src/models/Collection.ts
@@ -3,4 +3,13 @@ import { Eventing } from './Eventing';
 
 export class Collection {
     models: User[] = []
+    events: Eventing = new Eventing();
+
+    get on() {
+        return this.events.on;
+    }
+
+    get trigger() {
+        return this.events.trigger
+    }
 }
