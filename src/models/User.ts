fc0877e18031704878fb7631929ced5394cff239
Chad Hinds <chadrickj8@gmail.com>
2021-05-06 10:41:31 -0400

    Add a new method to User class that returns a passed in User

--- src/models/User.ts
+++ src/models/User.ts
@@ -2,7 +2,7 @@ import { Attributes } from './Attributes';
 import { ApiSync } from './ApiSync';
 import { Eventing } from './Eventing';
 import { AxiosResponse } from 'axios';
-
+import { Model } from './Model';
 
 export interface UserProps {
     // '?' marks the properties as optional
@@ -11,10 +11,15 @@ export interface UserProps {
     age?: number
 }
 const rooturl = 'http://localhost:3000/users'
-export class User {
-    public events: Eventing = new Eventing()
-    public sync: ApiSync<UserProps> = new ApiSync<UserProps>(rooturl);
-    public attributes: Attributes<UserProps>;
+
+export class User extends Model<UserProps> {
+    static buildBuild(attrs: UserProps): User {
+        return new User(
+            new Attributes<UserProps>(attrs),
+            new Eventing(),
+            new ApiSync<UserProps>(rooturl)
+        )
+    }
 
 
 }
