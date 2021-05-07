f2c061f1da7e087ce4c25a33c76d90fdaf542520
Chad Hinds <chadrickj8@gmail.com>
2021-05-07 09:14:09 -0400

    Make a folder to hold all views

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,3 @@
 import { Collection } from './models/Collection';
 import { User, UserProps } from './models/User';
 
-// const collection = 
-
-// collection.on('change', () => {
-//     console.log(collection)
-// })
-// collection.fetch()
