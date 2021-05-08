cd77a5932604d815b34f368998c44301f14a7e1a
Chad Hinds <chadrickj8@gmail.com>
2021-05-08 09:41:35 -0400

    Render user form to test if it works

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
-import { Collection } from './models/Collection';
-import { User, UserProps } from './models/User';
+import { UserForm } from './views/UserForm';
 
+const userForm = new UserForm(
+    document.getElementById('#root')
+)
+userForm.render();
