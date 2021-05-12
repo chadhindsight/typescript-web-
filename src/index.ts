0c35128c8b75fbf5967f76b32565c22c94a3109a
Chad Hinds <chadrickj8@gmail.com>
2021-05-12 18:24:25 -0400

    most recent update before refactor

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import { UserForm } from './views/UserForm';
+import { User } from './models/User';
 
+const user = User.buildUser({ name: 'Henry', age: 31 })
 const userForm = new UserForm(
-    document.getElementById('#root')
+    document.getElementById('#root'),
+    user
 )
 userForm.render();
