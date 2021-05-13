26c3c366825488c9fe893af13859f7ba6178a1df
Chad Hinds <chadrickj8@gmail.com>
2021-05-13 09:08:04 -0400

    Make UserForm extend The View class

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -1,16 +1,8 @@
 import { User } from '../models/User';
+import { View } from './View'
 
-export class UserForm {
-    constructor(public parent: Element, public model: User) {
-        this.bindModel()
-    }
+export class UserForm extends View {
 
-    bindModel() {
-        // call render method to update the view when data changes
-        this.model.on('change', () => {
-            this.render()
-        })
-    }
 
     eventsMap(): { [key: string]: () => void } {
         return {
@@ -48,28 +40,5 @@ export class UserForm {
         `
     }
 
-    // Helper method for render
-    bindEvents(fragment: DocumentFragment): void {
-        const eventsMap = this.eventsMap();
 
-        for (let eventKey in eventsMap) {
-            const [eventName, selector] = eventKey.split(':');
-
-            fragment.querySelectorAll(selector).forEach(element => {
-                element.addEventListener(eventName, eventsMap[eventKey])
-            })
-        }
-    }
-
-    // Takes our template and appends it to parent HTML element
-    render(): void {
-        this.parent.innerHTML = ''
-
-        const templateElement = document.createElement('template');
-        templateElement.innerHTML = this.template();
-
-        this.bindEvents(templateElement.content)
-
-        this.parent.append(templateElement.content)
-    }
 }
